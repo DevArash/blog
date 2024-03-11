312734561c256150452efdd159e4b0c2a53f915c
Arash Kazemieh <arash.kazemieh@gmail.com>
2024-03-11 22:59:24 +0330

    add route for about button

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -102,18 +102,14 @@
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">General</h6>
         </li>
-
-
         <li class="nav-item">
-          <a class="nav-link text-white @if ($pageName == 'About')bg-gradient-primary @endif" href="#">
+          <a class="nav-link text-white @if ($pageName == 'About')bg-gradient-primary @endif" href="{{route('dashboard.about')}}">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-symbols-outlined opacity-10">info</i>
             </div>
             <span class="nav-link-text ms-1">About</span>
           </a>
         </li>         
-
-
         <li class="nav-item">
           <a class="nav-link text-white @if ($pageName == 'Setting')bg-gradient-primary @endif" href="#">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
