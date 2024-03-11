3ec76338b7ba7bbd47327db2184b791dcb3f155c
Arash Kazemieh <arash.kazemieh@gmail.com>
2024-03-11 13:01:25 +0330

    change default routes to resource one

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\backend\postController;
 use App\Http\Controllers\frontend\homeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -28,25 +29,31 @@ Route::get('/contact', function(){
 
 // Backend Routs
 Route::middleware(['auth', 'verified'])->group(function () {
+    // load dashboard page
     Route::get('/dashboard',function(){
         return view('backend.dashboard.index')
             ->with('pageName','Dashboard');
     })->name('dashboard');
 
-    Route::get('/dashbord/posts',function(){
-        return view('backend.post.index')
-            ->with('pageName','Post');
-    })->name('dashboard.post');
+    // load posts list page
+    Route::get('/dashbord/posts',[postController::class,'index'])->name('dashboard.post');
+
+    // load edit post page for seleced post
+    Route::get('/dashbord/posts/edit/{id}',[postController::class,'edit'])->name('dashboard.post.edit');
+
+    // update selected post
+    Route::put('/dashbord/posts/edit/{id}/update',[postController::class,'update'])->name('dashboard.post.update');
+
+    // soft delete selected post 
+    Route::get('/dashboard/posts/edit/{id}/delete',[postController::class,'delete'])->name('dashboard.post.delete');
+
+
 
     Route::get('/dashbord/posts/create',function(){
         return view('backend.post.create')
             ->with('pageName','Create Post');
     })->name('dashboard.post.create');
 
-    Route::get('/dashbord/posts/edit',function(){
-        return view('backend.post.edit')
-            ->with('pageName','Edit Post');
-    })->name('dashboard.post.edit');
 });
 
 
