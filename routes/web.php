be3de467c7fe3bc214552f8682e2e44e24266c23
Arash Kazemieh <arash.kazemieh@gmail.com>
2024-03-14 18:47:50 +0330

    add settings routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\backend\aboutController;
 use App\Http\Controllers\backend\postController;
+use App\Http\Controllers\backend\settingController;
 use App\Http\Controllers\frontend\homeController;
 use App\Http\Controllers\ProfileController;
 use App\Models\about;
@@ -45,19 +46,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // Post Routes
     // load posts list page
     Route::get('/dashbord/posts',[postController::class,'index'])->name('dashboard.post');
-
     // load edit post page for seleced post
     Route::get('/dashbord/posts/edit/{id}',[postController::class,'edit'])->name('dashboard.post.edit');
-
     // update selected post
     Route::put('/dashbord/posts/edit/{id}/update',[postController::class,'update'])->name('dashboard.post.update');
-
     // soft delete selected post 
     Route::get('/dashboard/posts/edit/{id}/delete',[postController::class,'delete'])->name('dashboard.post.delete');
-
     // load create post page
     Route::get('/dashboard/posts/create',[postController::class,'create'])->name('dashboard.post.create');
-
     // create post
     Route::post('/dashboard/posts/store',[postController::class,'store'])->name('dashboard.post.store');
 
@@ -65,13 +61,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // About Routes
     // load about page
     Route::get('/dashboard/about',[aboutController::class,'index'])->name('dashboard.about');
-
     // create about
     Route::post('/dashboard/about/create',[aboutController::class,'store'])->name('dashboard.about.store');
-
     // update about
     Route::put('/dashbord/about/edit',[aboutController::class,'update'])->name('dashboard.about.update');
 
+
+    // Settings Route
+    Route::get('/dashboard/settings',[settingController::class,'index'])->name('dashboard.settings');
+    Route::put('/dashboard/settings/edit',[settingController::class,'update'])->name('dashboard.settings.update');
 });
 
 
