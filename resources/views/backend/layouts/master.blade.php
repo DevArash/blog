b809e63415df0ede123af4c67ad93702d789aa19
Arash Kazemieh <arash.kazemieh@gmail.com>
2024-03-14 23:41:32 +0330

    add trashed post button

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -80,6 +80,14 @@
             <span class="nav-link-text ms-1">Create Post</span>
           </a>
         </li> 
+        <li class="nav-item">
+          <a class="nav-link text-white @if ($pageName == 'Trashed Post')bg-gradient-primary @endif" href="{{route('dashboard.post.trashed')}}">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-symbols-outlined opacity-10">Recycling</i>
+            </div>
+            <span class="nav-link-text ms-1">Trashed Post</span>
+          </a>
+        </li> 
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Profile</h6>
         </li>
