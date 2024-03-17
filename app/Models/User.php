63817fb2b578eac6a768eb23c663248f7ab7af75
Arash Kazemieh <arash.kazemieh@gmail.com>
2024-03-17 11:03:05 +0330

    add some fields to user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        // add by me
+        'image',
+        'mobile',
+        'role',
+        'summary',
     ];
 
     /**
