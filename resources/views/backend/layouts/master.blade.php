bdef87d4ccea3a11e18251f193c8c406dc4b0ca9
Arash Kazemieh <arash.kazemieh@gmail.com>
2024-03-18 10:25:34 +0330

    fix create user button route

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -100,7 +100,7 @@
           </a>
         </li> 
         <li class="nav-item">
-          <a class="nav-link text-white @if ($pageName == 'Create User')bg-gradient-primary @endif" href="#">
+          <a class="nav-link text-white @if ($pageName == 'Create User')bg-gradient-primary @endif" href="{{route('dashboard.users.create')}}">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-symbols-outlined opacity-10">person_add</i>
             </div>
