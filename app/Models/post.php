6fdf1686df83cc937b1232b0b48cdb826cc67c9e
Arash Kazemieh <arash.kazemieh@gmail.com>
2024-03-18 10:24:44 +0330

    get user  of the post

--- app/Models/post.php
+++ app/Models/post.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class post extends Model
 {
     use softDeletes;
     use HasFactory;
     protected $fillable = ['image', 'topic', 'title', 'article'];
+
+
+    /**
+     * Get the user that owns the post.
+    */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
